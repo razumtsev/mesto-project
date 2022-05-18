64b3d767a241e8893406af47d4cbf932983dbb32
Ilya Razumtsev <razumtsev@mail.ru>
2022-05-18 19:55:53 +0300

    close button func added

--- vendor/script.js
+++ vendor/script.js
@@ -1,7 +1,16 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.button_type_edit');
 const addButton = document.querySelector('.button_type_add');
+const closeButton = document.querySelector('.button_type_close');
 
-editButton.addEventListener('click', function () {
+function openPopup () {
     popup.classList.add('popup_is-open');
-});
+}
+
+function closePopup () {
+    popup.classList.remove('popup_is-open');
+}
+
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
