df64d7b31ea6eb4f5d8995993bfbb1b61895b6af
Ilya Razumtsev <razumtsev@mail.ru>
2022-05-25 14:05:50 +0300

    cards created from array

--- vendor/script.js
+++ vendor/script.js
@@ -1,3 +1,30 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.button_type_edit');
 const addButton = document.querySelector('.button_type_add');
@@ -7,6 +34,31 @@ const profileOccupation = document.querySelector('.profile__occupation');
 const formEditProfile = document.querySelector('.form_type_edit-profile');
 const inputNickname = document.querySelector('.form__input_type_nickname');
 const inputOccupation = document.querySelector('.form__input_type_occupation');
+
+const elementsGrid = document.querySelector('.elements__grid');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function renderCard(card) {
+    elementsGrid.append(card);
+}
+
+function buildCard(name, link) {
+    const card = cardTemplate.querySelector('.elements__item').cloneNode(true);
+    card.querySelector('.element__image').src = link;
+    card.querySelector('.element__image').alt = name;
+    card.querySelector('.element__caption').textContent = name;
+    renderCard(card);
+}
+
+function arrHandler(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        buildCard(arr[i].name, arr[i].link);
+    }
+}
+
+arrHandler(initialCards);
+
+
 
 function openPopup () {
     popup.classList.add('popup_is-open');
